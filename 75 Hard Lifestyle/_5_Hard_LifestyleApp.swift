65cae4aa9d605e4fd6e9cf4e51bdf83eaba3c39f
jacksonhill7479 <111785662+jacksonhill7479@users.noreply.github.com>
2024-01-15 20:13:56 -0700

    Initial Commit

---
+++ 75 Hard Lifestyle/_5_Hard_LifestyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_Hard_LifestyleApp.swift
+//  75 Hard Lifestyle
+//
+//  Created by Jackson Hill on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct _5_Hard_LifestyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
